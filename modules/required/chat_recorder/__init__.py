78c51bdcb5ded08396be7f8328172a22203c2fb4
g1331 <1257661006@qq.com>
2023-01-14 15:44:26 +0800

    更新插件

--- modules/required/chat_recorder/__init__.py
+++ modules/required/chat_recorder/__init__.py
@@ -1,27 +1,32 @@
 import jieba
 import datetime
 
-from graia.ariadne.util.saya import listen
+from pathlib import Path
+
+from graia.ariadne.util.saya import listen, decorate
 from graia.saya import Saya, Channel
 from graia.ariadne.message.element import Plain
 from graia.ariadne.message.chain import MessageChain
 from graia.ariadne.event.message import Group, Member, GroupMessage
 
+from core.control import Distribute
+from core.models import saya_model
 from core.orm import orm
 from core.orm.tables import ChatRecord
 
 # 关闭 jieba 的 Debug log
 jieba.setLogLevel(jieba.logging.INFO)
-
+module_controller = saya_model.get_module_controller()
 saya = Saya.current()
 channel = Channel.current()
-
 channel.name("ChatRecorder")
 channel.author("SAGIRI-kawaii")
 channel.description("一个记录聊天记录的插件，可配合词云等插件使用")
+channel.metadata = module_controller.get_metadata_from_path(Path(__file__))
 
 
 @listen(GroupMessage)
+@decorate(Distribute.require())
 async def chat_record(message: MessageChain, group: Group, member: Member):
     content = "".join(plain.text for plain in message.get(Plain)).strip()
     seg_result = jieba.lcut(content) if content else ""

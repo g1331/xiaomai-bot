3ef8ac18282f6cee4849272921ebd09f919bcb2d
g1331 <1257661006@qq.com>
2023-01-21 00:17:54 +0800

    update

--- modules/self_contained/bf1_info/__init__.py
+++ modules/self_contained/bf1_info/__init__.py
@@ -3613,7 +3613,7 @@ async def join_handle(app: Ariadne, event: MemberJoinRequestEvent):
                     verify = "有效ID" if player_name else "无效ID"
             except:
                 verify = "无效ID"
-        application_answer = f"{application_answer}({verify})"
+        application_answer = f"{application_message}({verify})"
 
     # 如果有application_answer且verify为有效ID且开启了自动过审则自动同意
     if application_answer and verify == "有效ID" and \

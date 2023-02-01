f8a81533388f48e3a50957181c383521c6a85593
g1331 <1257661006@qq.com>
2023-02-01 22:47:12 +0800

    更新插件

--- modules/self_contained/av_bt/__init__.py
+++ modules/self_contained/av_bt/__init__.py
@@ -1,3 +1,4 @@
+import asyncio
 from pathlib import Path
 
 import aiohttp
@@ -10,23 +11,26 @@ from graia.ariadne.message.chain import MessageChain
 from graia.ariadne.message.element import Image
 from graia.ariadne.message.parser.twilight import Twilight, WildcardMatch, RegexResult, ArgumentMatch, ArgResult, \
     FullMatch
-from graia.ariadne.model.relationship import Group
+from graia.ariadne.model.relationship import Group, Member
 from graia.ariadne.util.saya import listen, decorate, dispatch
-from graia.saya import Channel
+from graia.broadcast.interrupt import InterruptControl
+from graia.saya import Channel, Saya
 from graiax.playwright import PlaywrightBrowser
 
 from core.config import GlobalConfig
 from core.control import Distribute, Function, Permission, FrequencyLimitation
 from core.models import saya_model
+from utils.waiter import ConfirmWaiter
 
 module_controller = saya_model.get_module_controller()
+saya = Saya.current()
 channel = Channel.current()
 channel.name("AVBT")
 channel.author("SAGIRI-kawaii")
 channel.author("移植by十三")
 channel.metadata = module_controller.get_metadata_from_path(Path(__file__))
 global_config = create(GlobalConfig)
-
+inc = InterruptControl(saya.broadcast)
 url = "https://sukebei.nyaa.si"
 proxy = global_config.proxy if global_config.proxy != "proxy" else None
 
@@ -46,7 +50,7 @@ proxy = global_config.proxy if global_config.proxy != "proxy" else None
     Permission.group_require(channel.metadata.level, if_noticed=True),
     Permission.user_require(Permission.User, if_noticed=False),
 )
-async def av_bt(app: Ariadne, group: Group, has_img: ArgResult, keyword: RegexResult, source: Source):
+async def av_bt(app: Ariadne, group: Group, has_img: ArgResult, keyword: RegexResult, source: Source, member: Member):
     keyword = keyword.result.display.strip()
     if not keyword:
         return
@@ -82,8 +86,14 @@ async def av_bt(app: Ariadne, group: Group, has_img: ArgResult, keyword: RegexRe
             await page.evaluate("document.getElementsByTagName('nav')[0].remove()")
             await page.evaluate("document.getElementsByTagName('footer')[0].remove()")
             if not has_img.matched:
-                await page.evaluate(
-                    "var a = document.getElementsByClassName('panel')[1].getElementsByTagName('img');while(a.length > 0){a[0].remove()}")
+                await page.evaluate("var a = document.getElementsByClassName('panel')[1].getElementsByTagName('img');while(a.length > 0){a[0].remove()}")
+            else:
+                await app.send_message(group, MessageChain(f"注意!该消息内容可能包含NSFW信息,是否继续查看?(y/n)"), quote=source)
+                try:
+                    if not await asyncio.wait_for(inc.wait(ConfirmWaiter(group, member)), 30):
+                        return await app.send_group_message(group, MessageChain("取消查看成功~"), quote=source)
+                except asyncio.TimeoutError:
+                    return await app.send_group_message(group, MessageChain("回复等待超时,进程退出"), quote=source)
             content = await page.screenshot(full_page=True)
         return await app.send_message(
             group, MessageChain([

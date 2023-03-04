d4762c652c39cafde7872c72c04a6858d81c1108
g1331 <1257661006@qq.com>
2023-03-05 01:06:57 +0800

    [ChatGPT] 更新权重

--- modules/self_contained/chat_gpt/__init__.py
+++ modules/self_contained/chat_gpt/__init__.py
@@ -215,7 +215,7 @@ manager = ConversationManager()
         decorators=[
             Distribute.require(),
             Function.require(channel.module),
-            FrequencyLimitation.require(channel.module, 7),
+            FrequencyLimitation.require(channel.module, 5),
             Permission.group_require(channel.metadata.level, if_noticed=True),
             Permission.user_require(Permission.User),
         ],

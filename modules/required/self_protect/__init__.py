d759b9252b72443a6442be5261cc5cf21941216e
g1331 <1257661006@qq.com>
2023-03-05 17:55:50 +0800

    [update] 自动同意管理的好友申请

--- modules/required/self_protect/__init__.py
+++ modules/required/self_protect/__init__.py
@@ -1,13 +1,14 @@
 from pathlib import Path
 
 from creart import create
-from graia.ariadne.event.mirai import BotMuteEvent
+from graia.ariadne.event.mirai import BotMuteEvent, NewFriendRequestEvent
 from graia.ariadne.message.chain import MessageChain
 from graia.ariadne.model import Group, Member
 from graia.ariadne.util.saya import listen
 from graia.saya import Channel, Saya
 
 from core.config import GlobalConfig
+from core.control import Permission
 from core.models import (
     saya_model,
     response_model
@@ -25,7 +26,7 @@ channel.metadata = module_controller.get_metadata_from_path(Path(__file__))
 
 
 @listen(BotMuteEvent)
-async def main(group: Group, member: Member):
+async def auto_quit_group(group: Group, member: Member):
     target_app, target_group = await account_controller.get_app_from_total_groups(global_config.test_group)
     if not (target_app and target_group):
         return
@@ -35,3 +36,15 @@ async def main(group: Group, member: Member):
         f"bot在{group.name}({group.id})被{member.name}({member.id})禁言\n"
         f"已自动退出该群!"
     ))
+
+
+@listen(NewFriendRequestEvent)
+async def auto_agree_admin_friend_request(event: NewFriendRequestEvent):
+    if (event.supplicant in await Permission.get_BotAdminsList()) or event.supplicant == global_config.Master:
+        target_app, target_group = await account_controller.get_app_from_total_groups(global_config.test_group)
+        if not (target_app and target_group):
+            return
+        return await target_app.send_message(target_group, MessageChain(
+            f"成员{event.nickname}({event.supplicant})申请添加BOT好友\n"
+            f'已自动同意该管理的申请'
+        ))

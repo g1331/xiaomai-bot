7dbf049b0ec3b479e3ac087f431e83823958c4d2
g1331 <1257661006@qq.com>
2023-03-08 16:24:02 +0800

    [ChatGPT] 更新对话的max tokens

--- modules/self_contained/chat_gpt/manager.py
+++ modules/self_contained/chat_gpt/manager.py
@@ -52,7 +52,7 @@ def get_gpt(preset="umaru"):
     return Chatbot(
         api_key=api_key,
         system_prompt=preset,
-        max_tokens=1000
+        max_tokens=2000
     )
     # return AsyncChatbot(config={
     #     "session_token": session_token

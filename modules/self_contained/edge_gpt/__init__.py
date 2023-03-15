d3865b2dd33b2376394f6b9adc5a27904c3dced0
g1331 <1257661006@qq.com>
2023-03-15 23:59:43 +0800

    bug fixed

--- modules/self_contained/edge_gpt/__init__.py
+++ modules/self_contained/edge_gpt/__init__.py
@@ -86,10 +86,8 @@ class ConversationManager(object):
             conversation_style = ConversationStyle.balanced if style == 1 else ConversationStyle.creative if style == 2 else ConversationStyle.precise
             response = (await self.data[group][member]["gpt"].ask(prompt="你好", conversation_style=conversation_style))
             result = response["item"]["messages"][1]["adaptiveCards"][0]["body"][0]["text"]
-            maxNumUserMessagesInConversation = response["item"]["messages"][8]["throttling"][
-                "maxNumUserMessagesInConversation"]
-            numUserMessagesInConversation = response["item"]["messages"][8]["throttling"][
-                "numUserMessagesInConversation"]
+            maxNumUserMessagesInConversation = response["item"]["throttling"]["maxNumUserMessagesInConversation"]
+            numUserMessagesInConversation = response["item"]["throttling"]["numUserMessagesInConversation"]
             result += f"\n\n(对话轮次:{numUserMessagesInConversation}/{maxNumUserMessagesInConversation})"
         except Exception as e:
             result = f"发生错误：{e}，请稍后再试"

7a52a3eff4590c60427010eb5d0e0a66e9c26792
IFV <96299096+Ra2-IFV@users.noreply.github.com>
2023-08-01 05:59:36 -0700

    Fix owner display id and pid

--- modules/self_contained/bf1_info/__init__.py
+++ modules/self_contained/bf1_info/__init__.py
@@ -1186,8 +1186,8 @@ async def detailed_server(
             f"创建时间: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(rspInfo['server']['createdDate']) / 1000))}\n"
             f"到期时间: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(rspInfo['server']['expirationDate']) / 1000))}\n"
             f"更新时间: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(rspInfo['server']['updatedDate']) / 1000))}\n"
-            f"服务器拥有者: {rspInfo.get('owner').get('name')}\n"
-            f"Pid: {rspInfo.get('owner').get('pid')}\n"
+            f"服务器拥有者: {rspInfo.get('owner').get('displayName')}\n"
+            f"Pid: {rspInfo.get('owner').get('personaId')}\n"
             + "=" * 20
         )
     if platoonInfo := server_info.get("platoonInfo"):

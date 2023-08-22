beb1e6a395abc73c9dd62a5ffb3031dfaf82a25b
十三 <84710711+g1331@users.noreply.github.com>
2023-08-23 01:44:15 +0800

    Bf1 refactor (#27)
    * refactor(bf1): 重构服管规划
    
    * refactor: bf1_server_manager 重构BF1群组管理
    
    * refactor: bf1_server_manager重构BF1群组管理
    
    * style: 格式化文件
    
    * update .gitignore
    
    * feat: Claude,通过slack与Claude对话
    
    * bug fixed
    
    * bug fixed: claude
    
    * style: BF1INFO格式化代码
    
    * update: Claude
    
    * update: Claude config
    
    * update: Claude
    
    * refactor: bf1_server_manager 重构BF1群组管理
    
    * refactor: bf1_server_manager重构BF1群组管理
    
    * refactor: BF1
    
    重构群组管理、服务器绑定与查询
    
    * 大更新
    
    * update: BF1ServerManager
    
    增删改查,整体进度20%
    
    * Revert "update: BF1ServerManager"
    
    This reverts commit 202ecc06
    
    * refactor(bf1): 重构服管规划
    
    * refactor: bf1_server_manager 重构BF1群组管理
    
    * refactor: bf1_server_manager重构BF1群组管理
    
    * style: 格式化文件
    
    * refactor: bf1_server_manager 重构BF1群组管理
    
    * refactor: bf1_server_manager重构BF1群组管理
    
    * refactor: BF1
    
    重构群组管理、服务器绑定与查询
    
    * 大更新
    
    * update: BF1ServerManager
    
    增删改查,整体进度20%
    
    * Revert "update: BF1ServerManager"
    
    This reverts commit 202ecc06
    
    * update: BF1ServerManager
    
    整理部分进度
    
    * update: BF1ServerManager
    
    更新BF1群组的权限控制
    
    * refactor(bf1): 重构服管规划
    
    * refactor: bf1_server_manager 重构BF1群组管理
    
    * refactor: bf1_server_manager重构BF1群组管理
    
    * style: 格式化文件
    
    * refactor: bf1_server_manager 重构BF1群组管理
    
    * refactor: bf1_server_manager重构BF1群组管理
    
    * refactor: BF1
    
    重构群组管理、服务器绑定与查询
    
    * 大更新
    
    * update: BF1ServerManager
    
    增删改查,整体进度20%
    
    * Revert "update: BF1ServerManager"
    
    This reverts commit 202ecc06
    
    * update: BF1ServerManager
    
    整理部分进度
    
    * update: BF1ServerManager
    
    更新BF1群组的权限控制
    
    * refactor(bf1): 重构服管规划
    
    * refactor: bf1_server_manager 重构BF1群组管理
    
    * refactor: bf1_server_manager重构BF1群组管理
    
    * style: 格式化文件
    
    * feat: Claude,通过slack与Claude对话
    
    * bug fixed
    
    * bug fixed: claude
    
    * update: Claude
    
    * update: Claude config
    
    * update: Claude
    
    * refactor: bf1_server_manager 重构BF1群组管理
    
    * refactor: bf1_server_manager重构BF1群组管理
    
    * refactor: BF1
    
    重构群组管理、服务器绑定与查询
    
    * 大更新
    
    * update: BF1ServerManager
    
    增删改查,整体进度20%
    
    * Revert "update: BF1ServerManager"
    
    This reverts commit 202ecc06
    
    * refactor: 战地一服务器管理
    
    重构内容:
    1.重构了群组管理、服管帐号管理
    2.重构了权限判断，将BF1群组权限判断与插件级权限判断分离,使得调用服管时可以跨群执行
    3.重构服管功能，包括踢出、封禁、解封、banall、 unbanall、换边、换图、上下v、清ban、自动清理征服过期vip
    4.重构、优化了大部分指令的逻辑
    
    待重构/优化内容:
    1.群组操作日志查询2.服务器配器修改
    3.玩家列表pic版优化重构
    4.Vban重构〔?真的有人在用么(
    5.自动更新gameid
    6.searchkick(模糊搜索踢出)重构

---
+++ utils/timeutils.py
@@ -0,0 +1,50 @@
+import datetime
+
+
+class DateTimeUtils:
+    @staticmethod
+    def add_days(start_date: datetime.datetime, days: int) -> datetime.datetime:
+        """
+        返回开始日期加上指定天数后的日期。
+
+        Args:
+        start_date (datetime.datetime): 开始日期。
+        days (int): 要添加的天数。
+
+        Returns:
+        datetime.datetime: 开始日期加上指定天数后的日期。
+
+        Raises:
+        ValueError: 如果 start_date 不是 datetime.datetime 对象，或者 days 不是整数。
+        """
+        if not isinstance(start_date, datetime.datetime):
+            raise ValueError("start_date must be a datetime.datetime object")
+        if not isinstance(days, int):
+            raise ValueError("days must be an integer")
+        return start_date + datetime.timedelta(days=days)
+
+    @staticmethod
+    def diff_days(date1: datetime.datetime, date2: datetime.datetime) -> int:
+        """
+        返回两个日期之间的天数差。
+
+        Args:
+        date1 (datetime.datetime): 第一个日期。
+        date2 (datetime.datetime): 第二个日期。
+
+        Returns:
+        int: 两个日期之间的天数差，使用date1 - date2。如果 date1 日期较晚（更大），返回值为正数；如果 date1 日期较早（更小），返回值为负数。
+
+        Raises:
+        ValueError: 如果 date1 或 date2 不是 datetime.datetime 对象。
+        """
+        if not isinstance(date1, datetime.datetime) or not isinstance(date2, datetime.datetime):
+            raise ValueError("Both date1 and date2 must be datetime.datetime objects")
+
+        # 将datetime对象转换为date对象
+        date1 = date1.date()
+        date2 = date2.date()
+
+        return (date1 - date2).days
+
+

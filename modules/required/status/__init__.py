94319228cb046d413eb2b2b5168d1ae7fbb370fc
g1331 <1257661006@qq.com>
2023-10-15 13:34:12 +0800

    refactor(status)：重构消息计数逻辑并增强代码文档
    - 将消息计数逻辑重构为一个专用的 `MessageCount` 类，抽象消息计数更新和检索逻辑。
    - 在消息计数更新方法中引入异步行为，确保非阻塞操作。
    - 使用 f-string 格式化与内存使用和消息计数相关的字符串输出，以获得更一致且可读的代码。
    - 添加详细的 docstrings 和注释，以提高代码的可读性和可维护性。
    - 实施错误处理和一致的代码格式化（在适用的地方）。
    
    这些更改旨在通过采用 OOP 实践和增强文档来提高代码的可维护性、可读性和一致性。

--- modules/required/status/__init__.py
+++ modules/required/status/__init__.py
@@ -1,7 +1,8 @@
 import time
 from datetime import datetime
 from pathlib import Path
-from typing import Union
+from typing import Union, Deque, Tuple
+from collections import deque
 
 import psutil
 from creart import create
@@ -41,15 +42,79 @@ channel.description("查询BOT运行状态")
 channel.author("13")
 channel.metadata = module_controller.get_metadata_from_path(Path(__file__))
 
-message_receive_before = 0
-message_send_before = 0
-Real_time_message_receive = 0
-Real_time_message_send = 0
+
+class MessageCount:
+    """
+    用于管理和检索消息计数统计信息的类。
+    """
+
+    def __init__(self):
+        """
+        初始化 MessageCount 实例。
+        """
+        self.receive_queue: Deque[Tuple[int, float]] = deque()
+        self.send_queue: Deque[Tuple[int, float]] = deque()
+
+    async def update_message_counts(self) -> None:
+        """
+        更新消息计数。
+
+        参数:
+        - core: 包含消息计数数据的对象。
+
+        注意:
+        - 为更新消息计数日志，应定期调用此方法。
+        """
+        self.receive_queue.append((core.received_count, time.time()))
+        self.send_queue.append((core.sent_count, time.time()))
+
+    def get_receive_count(self, time_limit: Union[int, float] = 60) -> int:
+        """
+        获取过去指定时间内接收到的消息数量。
+
+        参数:
+        - time_limit: Union[int, float], 指定的时间范围，单位为秒，默认为60秒。
+
+        返回:
+        - int: 过去指定时间内接收到的消息数量。
+        """
+        current_time = time.time()
+        # 从队列前端移除超过60秒的条目
+        while self.receive_queue and current_time - self.receive_queue[0][1] > time_limit:
+            self.receive_queue.popleft()
+
+        # 计算并返回过去一分钟内消息计数的差值
+        return self.receive_queue[-1][0] - self.receive_queue[0][0] if self.receive_queue else 0
+
+    def get_send_count(self, time_limit: Union[int, float] = 60) -> int:
+        """
+        获取过去指定时间内发送的消息数量。
+
+        参数:
+        - time_limit: Union[int, float], 指定的时间范围，单位为秒，默认为60秒。
+
+        返回:
+        - int: 过去指定时间内发送的消息数量。
+        """
+        current_time = time.time()
+        # 从队列前端移除超过60秒的条目
+        while self.send_queue and current_time - self.send_queue[0][1] > time_limit:
+            self.send_queue.popleft()
+
+        # 计算并返回过去一分钟内消息计数的差值
+        return self.send_queue[-1][0] - self.send_queue[0][0] if self.send_queue else 0
+
+
+message_count = MessageCount()
+
+
+@channel.use(SchedulerSchema(timers.every_custom_seconds(3)))
+async def message_counter():
+    await message_count.update_message_counts()
 
 
 # 接收事件
 @listen(GroupMessage, FriendMessage)
-# 依赖注入
 @decorate(
     Distribute.require(),
     Function.require(channel.module),
@@ -57,12 +122,10 @@ Real_time_message_send = 0
     Permission.group_require(channel.metadata.level, if_noticed=True),
     Permission.user_require(Permission.User, if_noticed=True),
 )
-# 消息链处理器
 @dispatch(Twilight([
     FullMatch("-bot").space(SpacePolicy.PRESERVE)
 ]))
 async def bot(app: Ariadne, src_place: Union[Group, Friend], source: Source):
-    global Real_time_message_receive, Real_time_message_send
     # 运行时长
     time_start = int(time.mktime(core.launch_time.timetuple()))
     m, s = divmod(int(time.time()) - time_start, 60)
@@ -79,31 +142,27 @@ async def bot(app: Ariadne, src_place: Union[Group, Friend], source: Source):
     # 内存占比
     zb = float(mem.percent)
     # cpu占用
-    zb2 = str(psutil.cpu_percent(interval=None, percpu=False)) + "%"
+    zb2 = f"{str(psutil.cpu_percent(interval=None, percpu=False))}%"
     # 磁盘
     cp = str(psutil.disk_usage('/').percent) + "%"
     launch_time = datetime.fromtimestamp(core.launch_time.timestamp()).strftime('%Y年%m月%d日%H时%M分%S秒')
-    await app.send_message(src_place, MessageChain(
-        f"开机时间：{launch_time}\n",
-        f"运行时长：{work_time}\n",
-        f"接收消息：{core.received_count}条(实时:%.1f/m)\n" % Real_time_message_receive,
-        f"发送消息：{core.sent_count + 1}条(实时:%.1f/m)\n" % Real_time_message_send,
-        f"内存使用：%.0fMB" % (ysy / 1024 / 1024),
-        f"(%.0f%%)\n" % zb,
-        f"CPU占比：{zb2}\n",
-        f"磁盘占比：{cp}\n",
-        f"在线bot数量:{len([app_item for app_item in core.apps if Ariadne.current(app_item.account).connection.status.available])}\n",
-        f"活动群组数量:{len(account_controller.total_groups.keys())}\n",
-        f"爱发电地址:https://afdian.net/a/ss1333\n",
-        f"项目地址:https://github.com/g1331/xiaomai-bot"
-    ), quote=source)
-
-
-# 自动刷新client
-@channel.use(SchedulerSchema(timers.every_custom_minutes(1)))
-async def message_counter():
-    global message_receive_before, message_send_before, Real_time_message_receive, Real_time_message_send
-    Real_time_message_receive = core.received_count - message_receive_before
-    Real_time_message_send = core.sent_count - message_send_before
-    message_receive_before = core.received_count
-    message_send_before = core.sent_count
+    real_time_received_message_count = message_count.get_receive_count()
+    real_time_sent_message_count = message_count.get_send_count()
+    await app.send_message(
+        src_place,
+        MessageChain(
+            f"开机时间：{launch_time}\n",
+            f"运行时长：{work_time}\n",
+            f"接收消息：{core.received_count}条(实时:{real_time_received_message_count}/m)\n"
+            f"发送消息：{core.sent_count + 1}条(实时:{real_time_sent_message_count}/m)\n"
+            f"内存使用：{ysy / 1024 / 1024:.0f}MB ({zb:.0f}%)\n",
+            f"CPU占比：{zb2}\n",
+            f"磁盘占比：{cp}\n",
+            f"在线bot数量:{len([app_item for app_item in core.apps if Ariadne.current(app_item.account).connection.status.available])}/"
+            f"{len(core.apps)}\n",
+            f"活动群组数量:{len(account_controller.total_groups.keys())}\n",
+            f"爱发电地址:https://afdian.net/a/ss1333\n",
+            "项目地址:https://github.com/g1331/xiaomai-bot",
+        ),
+        quote=source,
+    )

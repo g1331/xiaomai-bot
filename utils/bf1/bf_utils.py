94085203b7b2c70855b5390e5dce38e1647c0851
g1331 <1257661006@qq.com>
2023-11-05 12:24:03 +0800

    bugfix(BF1): 修复gt接口调用

--- utils/bf1/bf_utils.py
+++ utils/bf1/bf_utils.py
@@ -446,9 +446,12 @@ async def gt_bf1_stat() -> str:
 
 async def gt_get_player_id_by_name(player_name: str) -> Union[dict, None]:
     url = f"https://api.gametools.network/bf1/player/?name={player_name}&platform=pc&skip_battlelog=false"
+    headers = {
+        'accept': 'application/json'
+    }
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
+            async with session.get(url, headers=headers) as response:
                 response = await response.json()
         if response.get("errors"):
             logger.error(f"{player_name}|gt_get_player_id: {response['errors']}")
@@ -461,16 +464,20 @@ async def gt_get_player_id_by_name(player_name: str) -> Union[dict, None]:
 
 async def gt_get_player_id_by_pid(player_pid: str) -> Union[dict, None]:
     url = f"https://api.gametools.network/bf1/player/?playerid={player_pid}&platform=pc&skip_battlelog=false"
+    headers = {
+        'accept': 'application/json'
+    }
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
+            async with session.get(url, headers=headers) as response:
                 response = await response.json()
-        if response.get("errors"):
-            logger.error(f"{player_pid}|gt_get_player_id: {response['errors']}")
-            return None
+            if response.get("errors"):
+                logger.error(f"{player_pid}|gt_get_player_id: {response['errors']}")
+                return None
     except Exception as e:
         logger.error(f"gt_get_player_id: {e}")
         return None
+    return response
 
 
 async def check_vban(player_pid) -> dict or str:

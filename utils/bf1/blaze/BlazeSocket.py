c976c7042fb3fa3aeed09626560798eadb7c8dcf
g1331 <1257661006@qq.com>
2024-07-28 11:59:12 +0800

    update(Bf1Blaze)

--- utils/bf1/blaze/BlazeSocket.py
+++ utils/bf1/blaze/BlazeSocket.py
@@ -7,6 +7,8 @@ from loguru import logger
 
 from utils.bf1.blaze.Blaze import Blaze, keepalive
 
+# 允许不安全的 SSL 重新协商
+ssl.SSLContext().options |= ssl.OP_LEGACY_SERVER_CONNECT
 context = ssl.create_default_context()
 context.check_hostname = False
 context.verify_mode = ssl.CERT_NONE

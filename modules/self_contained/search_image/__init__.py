c19446aa34ede648eebd0afe1d3306ee7695f3b3
g1331 <1257661006@qq.com>
2024-08-01 13:31:16 +0800

    update(SearchImg): 暂时屏蔽`Ascii2D`的结果

--- modules/self_contained/search_image/__init__.py
+++ modules/self_contained/search_image/__init__.py
@@ -64,7 +64,7 @@ async def shiTu(app: Ariadne, group: Group, sender: Member, img: ElementResult, 
     # 使用方法搜索
     scrape_index_tasks = [
         asyncio.ensure_future(fun_saucenao(img_url)),
-        asyncio.ensure_future(fun_ascii2d(img_url)),
+        # asyncio.ensure_future(fun_ascii2d(img_url)),
         asyncio.ensure_future(fun_iqdb(img_url)),
         asyncio.ensure_future(fun_google(img_url)),
         asyncio.ensure_future(fun_ehentai(img_url)),

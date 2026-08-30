8debbdb004d7226d1fe9f5ef59f977569caef165
Umaru <caleb.1331@outlook.com>
2026-08-30 23:23:41 +0800

    chore(cleanup): remove legacy Graia code

--- core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-xiaomai-bot核心模块
-"""
-
-# 版本号（由bump-my-version自动管理）
-__version__ = "3.0.1"
--- utils/UI/__init__.py
+++
@@ -1,4 +0,0 @@
-from .IOS import gen as ios_gen  # noqa: F401
-from .IOS.models import *  # noqa: F401, F403
-from .models import *  # noqa: F401, F403
-from .OneMockUI import gen as one_mock_gen  # noqa: F401
